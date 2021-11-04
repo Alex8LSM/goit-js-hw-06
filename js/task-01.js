8d9a2dfe9efa1e3979cf1a4b7703a1ccdd4dc7fb
Oleksandr Khvaleba <alex8inbox@gmail.com>
2021-11-05 00:25:50 +0200

    Task1 Finish

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,7 @@
-const listWithItem = document.querySelector(".item");
-// listWithId.style.textTransform = 'uppercase';
-// listWithId.style.fontSize = '24px';
-const categoriesNumber = listWithItem.length;
-// console.log(`Number of categories: ${categoriesNumber}`);
-console.log(`list: ${listWithItem.ATTRIBUTE_NODE}`);
+const categories = document.querySelectorAll('.item');
+const categoriesNumber = categories.length;
+console.log(`Number of categories: ${categoriesNumber}`);
+for (const category of categories) {
+    console.log(`Category: ${category.firstElementChild.textContent}`);
+    console.log(`Elements: ${category.lastElementChild.childElementCount}`);
+}
