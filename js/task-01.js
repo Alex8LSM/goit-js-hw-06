d0e63e1900d2a35a9bdec116a72b7e8f264279d1
Oleksandr <alex8inbox@gmail.com>
2021-11-04 17:28:55 +0200

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,6 @@
+const listWithItem = document.querySelector(".item");
+// listWithId.style.textTransform = 'uppercase';
+// listWithId.style.fontSize = '24px';
+const categoriesNumber = listWithItem.length;
+// console.log(`Number of categories: ${categoriesNumber}`);
+console.log(`list: ${listWithItem.ATTRIBUTE_NODE}`);
